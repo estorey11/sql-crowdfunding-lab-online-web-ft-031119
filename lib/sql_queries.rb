e8a4a5bf0ade22d68b736dadbe7e023f836c5feb
Eric Storey <estorey@gmail.com>
2019-04-12 18:53:34 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,8 +10,8 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT projects.title, SUM(pledges.amount)
 FROM projects
 LEFT OUTER JOIN pledges
-ON projects.id = pledges.
-;"
+ON projects.id = pledges.project_id
+ORDER BY projects.name;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
